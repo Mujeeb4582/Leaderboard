91f9cf3befb59fc5f809873fae9d7c5a57c17712
Mujeeb4582 <mujeeburahman4582@gmail.com>
2022-11-17 16:07:57 +0500

    Add the api url and key

---
+++ src/modules/api.js
@@ -0,0 +1,5 @@
+import poplauteTable from './populateTable.js';
+
+const baseUrl = 'https://us-central1-js-capstone-backend.cloudfunctions.net/api/games/';
+const apiKey = 'gPaICDgdmVhqNJQa3k5i';
+const gameUrl = `${baseUrl}${apiKey}/scores/`;
